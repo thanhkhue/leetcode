2ae48fbd55b7c55fcaac7ce24b0cbae7cd5dca40
Khue Bui <khue.bui@be.com.vn>
2022-01-01 14:40:23 +0700

    Number of pairs of interchangeable rectangles

---
+++ number-of-pairs-of-interchangeable-rectangles/main.go
@@ -0,0 +1,13 @@
+package main
+
+func interchangeableRectangles(rectangles [][]int) int64 {
+	ratio := make(map[float64]int)
+	res := 0
+	for i := 0; i < len(rectangles); i++ {
+		res += ratio[float64(rectangles[i][0])/float64(rectangles[i][1])]
+		ratio[float64(rectangles[i][0])/float64(rectangles[i][1])]++
+	}
+
+	return int64(res)
+
+}
